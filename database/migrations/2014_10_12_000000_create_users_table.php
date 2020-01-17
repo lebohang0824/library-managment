e23b77f7ccfa98e20a60de2daba6909d05fe0075
Lebohang <lebohang0824@gmail.com>
2020-01-17 12:40:21 +0200

    Scheme length added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('surname');
             $table->string('cohort');
             $table->string('department');
-            $table->string('email')->unique();
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
